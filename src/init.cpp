607cbbc56c7eb912a70cec8bd122c1ca211e53ac
Marco A <marco.diiga@gmail.com>
2016-01-05 20:09:18 +0100

    Merged changes from https://github.com/markD9/Varco.git

--- src/init.cpp
+++ src/init.cpp
@@ -1,11 +1,12 @@
 #ifdef WIN32
+  #include <WindowHandling/MainWindow.hpp>
   #include "windows.h"
 #endif
-#include <WindowHandling/MainWindow.hpp>
 
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
 {
-  return varco::MainWindow::create(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+  varco::MainWindow window(hInstance, hPrevInstance, lpCmdLine, nCmdShow);  
+  return window.show();
 }
 #endif
